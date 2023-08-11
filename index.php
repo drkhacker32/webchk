5f7803d24146a3f4e470289b4f9d8753e4ee1364
drkhacker32 <138890195+drkhacker32@users.noreply.github.com>
2023-08-11 15:20:43 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,450 @@
+
+<!DOCTYPE html>
+<html class="loading">
+  <head>
+	<meta name="author" content="Doctor">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">    
+    <title> BAD BOY</title>
+	<link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,300i,400,400i,600,600i,700,700i%7CComfortaa:300,400,700" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="theme-assets/css/vendors.css">
+    <link rel="stylesheet" type="text/css" href="theme-assets/css/app-lite.css">
+    <link rel="stylesheet" type="text/css" href="theme-assets/css/core/menu/menu-types/vertical-menu.css">
+    <link rel="stylesheet" type="text/css" href="theme-assets/css/core/colors/palette-gradient.css">
+	<link rel="shortcut icon" type="image/x-icon" href="Exceed-Logo.ico"/>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+  </head>
+
+
+
+  
+  <body class="vertical-layout" style="background-color:#141414;" data-color="bg-gradient-x-purple-blue">   
+  <style> 
+		h5,h4{
+			color:white;
+		}
+		.text-center{
+			background-color:#2C2C2C;
+			border:1px solid #525252;
+			border-radius:5px;
+			font-family: 'Bebas Neue', cursive;
+			letter-spacing: 2px;
+		}
+		textarea{
+			color:white;
+			resize: none;
+		}
+
+		.text-center::placeholder{
+			color:grey;
+		}
+		.text-center:focus{
+			background-color:#2C2C2C;
+		}
+
+		textarea::-webkit-scrollbar {
+  			width: 5px;
+ 			background-color: #112132; 
+		}
+
+		textarea::-webkit-scrollbar-thumb {
+ 			border-radius: 10px;
+  			background-color: #2e4964; 
+		}
+		.lista_reprovadass{
+			color:#747474;
+		}
+		.card-body{
+			background-color: #1D1D1D; 
+			border-radius:5px;
+		}
+		.text-center{
+			border:none;
+		}
+		.badge-success,.btn-success{
+			background-color: #5ed84f;
+			color:black	;
+			border:none;
+		}
+		.btn-success:hover{
+			background-color: #28af17;
+			border:none;
+			color:black;
+			shadow:hidden;
+		}
+		.aprovadas{
+			background-color: #35a7ff;
+			color:black	;
+		}
+		.badge-danger{
+			background-color: #ff1b23;
+			color:black	;
+		}
+		.html body .content .content-wrapper{
+			background-color:#112132;
+		}
+
+		.btn-bg-gradient {
+			background-image: linear-gradient(to right, #1FA2FF 0%, #12D8FA  51%, #1FA2FF  100%);
+   			margin: 5px;
+			width:49%;
+    		padding: 12px 40px;
+    		text-align: center;
+    		text-transform: uppercase;
+    		transition: 0.5s;
+    		background-size: 200% auto;
+    		color: white;            
+    		box-shadow: 0 0 20px #eee;
+    		border-radius: 5px;
+    		display: block;
+			-webkit-box-shadow: 0 0 0 0 #514a9d;
+			font-family: 'Fjalla One', sans-serif;
+			letter-spacing: 2px;
+  		}
+
+  		.btn-bg-gradient:hover {
+   			background-position: right center;
+    		color: #fff;
+    		text-decoration: none;
+  		}
+
+		  .btn-bg-gradient-x {
+			background-image: linear-gradient(to right, #CB356B 0%, #BD3F32  51%, #CB356B  100%);
+            margin: 5px;
+            padding: 12px 45px;
+			cursor: pointer;
+            text-align: center;
+            text-transform: uppercase;
+            transition: 0.5s;
+            background-size: 200% auto;
+            color: white;            
+            box-shadow: 0 0 20px #eee;
+            border-radius: 5px;
+            display: block;
+			-webkit-box-shadow: 0 0 0 0 #514a9d;
+			font-family: 'Fjalla One', sans-serif;
+			letter-spacing: 2px;
+  		}
+
+  		.btn-bg-gradient-x:hover {
+			background-position: right center;
+            color: #fff;
+            text-decoration: none;
+  		}
+
+		  .statusbar{
+			height:320px;
+			padding-top:50px;
+		  }
+		  .hr-statusbar{
+			border:none;
+			height:1px;
+			background-image: linear-gradient(to right, #007fa9 , #25289e);
+		  }
+		  .footer-C {
+			color:white;
+			text-align: center;
+			font-family: 'Bebas Neue', cursive;
+			font-size: 17px;
+			letter-spacing: 1px;
+		  }
+	</style>	
+	<!-- Header IMG : https://i.ibb.co/Lrm15dD/Exceed-Logo-white.png -->
+	<img href="https://i.ibb.co/Lrm15dD/Exceed-Logo-white.png" src="https://i.ibb.co/Lrm15dD/Exceed-Logo-white.png" alt="Exceed-Logo-white" border="0" style="
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 31px;
+  height: 53px;
+  margin-top: 15px;
+	">
+
+	
+ 
+ <div class="app-content content" style="display:block;">
+      <div class="content-wrapper" style="background-color:#141414;">
+             
+  <div class="content-body" >
+  	<div class="mt-2"></div>
+	<div class="row" style="max-width: 700px; margin: auto;">    
+		<div class="col-md-8">
+			<div class="card" style="background-color:transparent;">
+				<div class="card-body text-center" style="">
+					<h4 class="mb-2"><strong> BAD BOY </strong></h4>
+                    <a  
+					style="width:140px;"
+					></a>
+					</h4>
+					<textarea rows="8" class="form-control text-center form-checker mb-2" placeholder="XXXXXXXXXXXXXXXX|XX|XX|XXX" style="color:#ffffff94"></textarea>
+					<textarea rows="1" class="form-control text-center" style="width: 70%; float: left ;"  id="sec" placeholder="sk empty for non sk"></textarea>
+					<textarea rows="1"class="form-control text-center" style="width: 30%; float: right margin-bottom: 5px;" id="cst" placeholder="Custom Amount"></textarea></br>
+					<select name="gate" id="gate" class="form-control" style="margin-top: 0px;">
+					<option style="background:rgba(165, 154, 154, 0.281);color:rgb(255, 208, 0);color:white" value="gate/cvv.php">𝗖𝗩𝗩 𝗖𝗛𝗔𝗥𝗚𝗘𝗗 $𝟭</option>
+					<option style="background:rgba(165, 154, 154, 0.281);color:rgb(255, 208, 0);color:white" value="gate/ccn.php">CCN 𝗖𝗛𝗔𝗥𝗚𝗘𝗗 €𝟭</option>
+						
+                </select>
+					<button class="btn btn-play  btn-bg-gradient text-white" style="width: 45%; float: left; "><i class="fa fa-play" style="margin-right: 3px;"></i>START</button>
+					<button class="btn btn-stop  btn-bg-gradient-x text-white" style="width: 45%; float: right;" disabled><i class="fa fa-stop" style="margin-right: 3px;"></i>STOP</button>
+
+				</div>
+			</div>
+		</div>
+		
+<div class="col-md-4">
+  <div class="card mb-2" style="background-color:transparent;">
+  	<div class="card-body statusbar" >    
+	
+<h5 >CHARGED :<span class="badge badge-success float-right charge">0</span></h5><hr class="hr-statusbar">
+
+<h5>LIVE :<span class="badge badge-success float-right aprovadas">0</span></h5><hr class="hr-statusbar">
+
+<h5>DEAD :<span class="badge badge-danger float-right reprovadas">0</span></h5><hr class="hr-statusbar">
+
+<h5>TOTAL :<span class="badge badge-primary float-right carregadas">0</span></h5><hr class="hr-statusbar">
+
+<h5>LIMIT :<span class="badge badge-secondary float-right">2000</span></h5>
+
+                                    <label class="form-control-label" style="margin-left: 10px; margin-bottom: 20px" for="inputcvv"></label>
+                  </div> 
+                </div>
+              </div>
+			  
+            		<div class="col-xl-12">
+			<div class="card" style="background-color:transparent;">
+				<div class="card-body" >
+					<div class="float-right">
+						<button type="show" class="btn btn-primary btn-sm show-charge"><i class="fa fa-eye-slash"></i></button>
+					<button class="btn btn-success btn-sm btn-copy1"><i class="fa fa-copy"></i></button>					
+					</div>
+					<h4 class="card-title mb-1"><i class="fa fa-check-circle text-success"></i> CHARGED</h4>					
+			<div id='lista_charge'></div>
+				</div>				
+			</div>
+		</div>
+		<div class="col-xl-12">
+			<div class="card" style="background-color:transparent;">
+				<div class="card-body" >
+					<div class="float-right">
+						<button type="show" class="btn btn-primary btn-sm show-lives"><i class="fa fa-eye-slash"></i></button>
+					<button class="btn btn-success btn-sm btn-copy"><i class="fa fa-copy"></i></button>					
+					</div>
+					<h4 class="card-title mb-1"><i class="fa fa-check text-success"></i> CVV/CCN</h4>					
+			<div id='lista_aprovadas'></div>
+				</div>				
+			</div>
+		</div>
+		<div class="col-xl-12">
+			<div class="card" style="background-color:transparent;">
+				<div class="card-body" >
+					<div class="float-right">
+						<button type='hidden' class="btn btn-primary btn-sm show-dies"><i class="fa fa-eye"></i></button>
+					<button class="btn btn-danger btn-sm btn-trash"><i class="fa fa-trash"></i></button>					
+					</div>
+					<h4 class="card-title mb-1"><i class="fa fa-times text-danger"></i> DEAD</h4>		
+						<div style='display: none;' id='lista_reprovadas' class="lista_reprovadass"><br></div>
+				</div>				
+			</div>
+		</div>
+		
+</section>
+        </div>
+      </div>
+    </div>
+ <div class="footer-C">
+	<span>CRAFTED BY </span><a href="https://t.me/ccChecker_scripts" target="b_lank" style="
+    background: -webkit-linear-gradient(#1591ff, #00f2fe);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+	cursor: pointer;
+	"><span>T-rex</span></a>
+ </div>
+
+ <noscript>
+ <p>🇸‌🇴‌🇷‌🇷‌🇾‌ 🇾‌🇴‌🇺‌🇷‌ 🇧‌🇷‌🇴‌🇼‌🇸‌🇪‌🇷‌ 🇩‌🇴‌🇪‌🇸‌🇪‌🇳‌'🇹‌ 🇸‌🇺‌🇵‌🇵‌🇴‌🇷‌🇹‌ 🇴‌🇺‌🇷‌ 🇯‌🇸‌ 🇻‌🇪‌🇷‌🇸‌🇮‌🇴‌🇳‌ :) 🇵‌🇱‌🇪‌🇦‌🇸‌🇪‌ 🇺‌🇸‌🇪‌ 🇺‌🇵‌🇩‌🇦‌🇹‌🇪‌🇩‌ 🇧‌🇷‌🇴‌🇼‌🇸‌🇪‌🇷‌!</p>
+<span style="font-family:sans-serif;color:black;font-size:25px;">Here some cool browsers to be updated :</span>
+<pre style="font-family:sans-serif;color:black;font-size:30px;">
+	<a href="https://www.google.com/chrome" target="b_lank">Chrome</a> | <a href="https://www.mozilla.org/" target="b_lank">FireFox</a> | <a href="https://www.opera.com/" target="b_lank">Opera</a> | <a href="https://brave.com/" target="b_lank">Brave</a>
+</pre>
+ </noscript>
+
+    <script src="theme-assets/js/core/libraries/jquery.min.js" type="text/javascript"></script>
+
+<script>
+
+$(document).ready(function(){
+
+
+
+
+$('.show-charge').click(function(){
+var type = $('.show-charge').attr('type');
+$('#lista_charge').slideToggle();
+if(type == 'show'){
+$('.show-charge').html('<i class="fa fa-eye"></i>');
+$('.show-charge').attr('type', 'hidden');
+}else{
+$('.show-charge').html('<i class="fa fa-eye-slash"></i>');
+$('.show-charge').attr('type', 'show');
+}});
+
+$('.show-lives').click(function(){
+var type = $('.show-lives').attr('type');
+$('#lista_aprovadas').slideToggle();
+if(type == 'show'){
+$('.show-lives').html('<i class="fa fa-eye"></i>');
+$('.show-lives').attr('type', 'hidden');
+}else{
+$('.show-lives').html('<i class="fa fa-eye-slash"></i>');
+$('.show-lives').attr('type', 'show');
+}});
+
+$('.show-dies').click(function(){
+var type = $('.show-dies').attr('type');
+$('#lista_reprovadas').slideToggle();
+if(type == 'show'){
+$('.show-dies').html('<i class="fa fa-eye"></i>');
+$('.show-dies').attr('type', 'hidden');
+}else{
+$('.show-dies').html('<i class="fa fa-eye-slash"></i>');
+$('.show-dies').attr('type', 'show');
+}});
+
+$('.btn-trash').click(function(){
+	Swal.fire({title: 'REMOVE DIE CCs SUCCESS', icon: 'success', showConfirmButton: false, toast: true, position: 'top-end', timer: 3000});
+$('#lista_reprovadas').text('');
+});
+
+$('.btn-copy1').click(function(){
+	Swal.fire({title: 'COPY CHARGED CCs SUCCESS', icon: 'success', showConfirmButton: false, toast: true, position: 'top-end', timer: 3000});
+var lista_charge = document.getElementById('lista_charge').innerText;
+var textarea = document.createElement("textarea");
+textarea.value = lista_charge;
+document.body.appendChild(textarea); 
+textarea.select(); 
+document.execCommand('copy');           document.body.removeChild(textarea); 
+});
+
+
+$('.btn-copy').click(function(){
+	Swal.fire({title: 'COPY LIVE CCs SUCCESS', icon: 'success', showConfirmButton: false, toast: true, position: 'top-end', timer: 3000});
+var lista_lives = document.getElementById('lista_aprovadas').innerText;
+var textarea = document.createElement("textarea");
+textarea.value = lista_lives;
+document.body.appendChild(textarea); 
+textarea.select(); 
+document.execCommand('copy');           document.body.removeChild(textarea); 
+});
+
+
+$('.btn-play').click(function(){
+var sec = $("#sec").val();
+var cst = $("#cst").val();
+var e = document.getElementById("gate");
+var gate = e.options[e.selectedIndex].value;
+var lista = $('.form-checker').val().trim();
+var array = lista.split('\n');
+var charge = 0, lives = 0, dies = 0, testadas = 0, txt = '';
+
+if(!lista){
+	Swal.fire({title: 'Where your card?? please add a card!!', icon: 'error', showConfirmButton: false, toast: true, position: 'top-end', timer: 3000});
+	return false;
+}
+
+Swal.fire({title: 'Please wait for the card to be processed !!', icon: 'success', showConfirmButton: false, toast: true, position: 'top-end', timer: 3000});
+
+var line = array.filter(function(value){
+if(value.trim() !== ""){
+	txt += value.trim() + '\n';
+	return value.trim();
+}
+});
+
+/*
+var line = array.filter(function(value){
+return(value.trim() !== "");
+});
+*/
+
+var total = line.length;
+
+
+/*
+line.forEach(function(value){
+txt += value + '\n';
+});
+*/
+
+$('.form-checker').val(txt.trim());
+if(total > 2000){
+  Swal.fire({title: 'Please do not check more than 2000 CCs :)', icon: 'warning', showConfirmButton: false, toast: true, position: 'top-end', timer: 3000});
+  return false;
+}
+
+$('.carregadas').text(total);
+$('.btn-play').attr('disabled', true);
+$('.btn-stop').attr('disabled', false);
+
+line.forEach(function(data){
+var callBack = $.ajax({
+	url: gate + '?lista=' + data + '&sec=' + sec,
+	success: function(retorno){
+		if(retorno.indexOf("#CHARGED") >= 0){
+			Swal.fire({title: '+1 CHARGED CC', icon: 'success', showConfirmButton: false, toast: true, position: 'top-end', timer: 3000});
+			$('#lista_charge').append(retorno);
+			removelinha();
+			charge = charge +1;
+			}
+			else if(retorno.indexOf("#LIVE") >= 0){
+			Swal.fire({title: '+1 LIVE CC', icon: 'success', showConfirmButton: false, toast: true, position: 'top-end', timer: 3000});
+			$('#lista_aprovadas').append(retorno);
+			removelinha();
+			lives = lives +1;
+		    }else{
+			$('#lista_reprovadas').append(retorno);
+			removelinha();
+			dies = dies +1;
+		}
+		testadas = charge + lives + dies;
+	    $('.charge').text(charge);
+		$('.aprovadas').text(lives);
+		$('.reprovadas').text(dies);
+		$('.testadas').text(testadas);
+		
+		if(testadas == total){
+			Swal.fire({title: 'HAVE BEEN DISPOSED', icon: 'success', showConfirmButton: false, toast: true, position: 'top-end', timer: 3000});
+			$('.btn-play').attr('disabled', false);
+			$('.btn-stop').attr('disabled', true);
+		}
+        }
+      });
+      $('.btn-stop').click(function(){
+      Swal.fire({title: 'Succeeding Pause !!', icon: 'warning', showConfirmButton: false, toast: true, position: 'top-end', timer: 3000});
+      $('.btn-play').attr('disabled', false);
+      $('.btn-stop').attr('disabled', true);      
+      	callBack.abort();
+      	return false;
+      });
+    });
+  });
+});
+
+function removelinha() {
+var lines = $('.form-checker').val().split('\n');
+lines.splice(0, 1);
+$('.form-checker').val(lines.join("\n"));
+}
+
+
+  
+	
+</script>
+
+  </body>
+</html>
